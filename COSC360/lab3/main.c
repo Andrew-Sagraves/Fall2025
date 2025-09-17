142025b7378d50831af58b5e333e9133af87d970
asagrave <andysagraves@gmail.com>
2025-09-16 20:10:44 -0400

    cosc360 proj3 done

--- COSC360/lab3/main.c
+++ COSC360/lab3/main.c
@@ -1,10 +1,48 @@
-// This file will NOT be graded.
-#include <stdio.h>
 #include "ring.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main()
-{
-    printf("Hello, World!\n");
-    return 0;
+#define CAP 20
+
+int main() {
+  struct RingBuffer *rb = rb_new(CAP);
+  char bytes[CAP];
+  size_t n;
+  char c;
+
+  rb_push(rb, 'A');
+  rb_push(rb, 'l');
+  rb_push(rb, 'p');
+  rb_push(rb, 'h');
+  rb_push(rb, 'a');
+  rb_push(rb, 'D');
+  rb_push(rb, 'o');
+  rb_push(rb, 'g');
+  char *buf = malloc(100 * sizeof(char));
+  rb_read(rb, buf, 100);
+  printf("%s", buf);
+  printf("\n");
+  rb_ignore(rb, 2);
+  char *buf2 = malloc(100 * sizeof(char));
+  rb_read(rb, buf2, 100);
+  printf("%s", buf2);
+  printf("\n");
+  /// printf("rb_pop: popped '%c'\n", c);
+
+  /// n = rb_read(rb, bytes, CAP + 5);
+
+  /// printf("rb_read: returned %lu bytes '%.*s'\n", n, n, bytes);
+
+  /// n = rb_write(rb, "Hello World", strlen("Hello World"));
+
+  /// printf("rb_write: wrote %lu bytes.\n", n);
+
+  /// n = rb_read(rb, bytes, CAP);
+
+  /// printf("rb_read: returned %lu bytes '%.*s'\n", n, n, bytes);
+
+  /// rb_free(rb);
+
+  /// return 0;
 }
-
--- COSC360/lab3/ring.c
+++ COSC360/lab3/ring.c
@@ -1,4 +1,100 @@
 #include "ring.h"
+#include <string.h>
+typedef struct RingBuffer {
+  size_t at;       // index of next byte
+  size_t size;     // total number of unread bytes
+  size_t capacity; // total number of bytes in this buffer
+  char *buffer;    // data storage, use calloc() with capacity
 
-// Write your ring buffer functions here
+} RingBuffer;
+
+RingBuffer *rb_new(size_t capacity) {
+  struct RingBuffer *rb = malloc(sizeof(RingBuffer));
+  rb->at = 0;
+  rb->size = 0;
+  rb->capacity = capacity;
+  rb->buffer = calloc(capacity, sizeof(size_t));
+  return rb;
+}
+void rb_free(RingBuffer *rb) {
+  free(rb->buffer);
+  free(rb);
+}
+
+// Ring buffer components
+size_t rb_at(const RingBuffer *rb) { return rb->at; }
+
+size_t rb_size(const RingBuffer *rb) { return rb->size; }
+
+size_t rb_capacity(const RingBuffer *rb) { return rb->capacity; }
+
+bool rb_push(RingBuffer *rb, char data) {
+  if (rb_size(rb) >= rb_capacity(rb)) {
+    return false;
+  }
+  int slot = (rb_at(rb) + rb_size(rb)) % rb_capacity(rb);
+  rb->buffer[slot] = data;
+  rb->size += 1;
+  return true;
+}
+bool rb_pop(RingBuffer *rb, char *data) {
+  if (rb->size <= 0) {
+    return false;
+  }
+  *data = rb->buffer[rb_at(rb)];
+  rb->at += 1;
+  rb->size -= 1;
+  return true;
+}
+char rb_peek(const RingBuffer *rb) { return rb->buffer[rb_at(rb)]; }
+
+void rb_ignore(RingBuffer *rb, size_t num) {
+  if (num > rb->size) {
+    num = rb->size;
+  }
+  rb->at = (rb->at + num) % rb->capacity;
+  rb->size -= num;
+}
+void rb_clear(RingBuffer *rb) {
+  rb->at = 0;
+  rb->size = 0;
+}
+
+size_t rb_read(RingBuffer *rb, char *buf, size_t max_bytes) {
+    if (max_bytes >= rb->size) {
+        max_bytes = rb->size;
+    }
+
+    if (rb->at + max_bytes > rb->capacity) {
+        int rest = rb->at + max_bytes - rb->capacity;
+        int init = rb->capacity - rb->at;
+
+        memcpy(buf, rb->buffer + rb->at, init * sizeof(char));
+        memcpy(buf + init, rb->buffer, rest * sizeof(char));
+        return max_bytes;
+    }
+
+    memcpy(buf, rb->buffer + rb->at, max_bytes * sizeof(char));
+    return max_bytes;
+}
+size_t rb_write(struct RingBuffer *rb, const char *buf, size_t max_bytes) {
+    
+if (max_bytes + rb->size >= rb->capacity) {
+        max_bytes = rb->capacity - rb->size;
+    }
+
+    int start = rb->at + rb->size % rb->capacity;
+
+    if (start + max_bytes > rb->capacity) {
+        int rest = start + max_bytes - rb->capacity;
+        int init = rb->capacity - start;
+
+        memcpy(rb->buffer + start, buf,  init * sizeof(char));
+        memcpy(rb->buffer, buf + init, rest * sizeof(char));
+        return max_bytes;
+    }
+
+    memcpy(rb->buffer + start, buf, max_bytes * sizeof(char));
+    return max_bytes;
+}
 
